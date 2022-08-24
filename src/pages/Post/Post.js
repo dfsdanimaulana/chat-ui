e7e3d642040ceed14e93754014394856d838441d
dfsdanimaulana <danimaulana9f@gmail.com>
2022-08-24 23:53:32 +0700

    upload muultiple image done

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -1,21 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Avatar from '../../components/Avatar/Avatar'
 import useWindowDimensions from '../../hooks/useWindowDimensions'
+import BASE_URL from '../../config'
+import axios from 'axios'
 
 export default function Post() {
     const { width } = useWindowDimensions()
-    const [imgSrc, setImgSrc] = useState('https://picsum.photos/400')
+    const [imgSrc, setImgSrc] = useState([
+        'https://i.ibb.co/g3ffFKB/camera.png',
+    ])
     const [error, setError] = useState(false)
     const [isPending, setIsPending] = useState(false)
     const [post, setPost] = useState({
-        userId: 'currentUser._id',
-        username: 'currentUser.username',
-        caption: '',
-        hashtag: '',
-        image: '',
+        userId: '63049de40a8779bd2b154792',
+        username: 'novi',
+        caption: 'im testing upload multiple images',
+        hashtag: 'multiple image',
+        image: [],
     })
 
     const imageStyle = (w) => {
+        const size = w >= 768 ? 300 : 200
+        return {
+            width: `${size}px`,
+            height: `${size}px`,
+            objectFit: 'cover',
+        }
+    }
+
+    const carouselStyle = (w) => {
         const size = w >= 768 ? 300 : 200
         return {
             width: `${size}px`,
@@ -24,8 +37,29 @@ export default function Post() {
     }
 
     const handleSubmit = (e) => {
+        setError(false)
+        setIsPending(true)
         e.preventDefault()
-        console.log(post)
+
+        if (post.image.length < 1) {
+            setError('Please select an image')
+            return
+        }
+
+        axios
+            .post(`${BASE_URL}/post`, post)
+            .then((res) => {
+                setIsPending(false)
+                // history.push('/')
+                console.log(res.data)
+                setImgSrc(['https://i.ibb.co/g3ffFKB/camera.png'])
+            })
+            .catch((err) => {
+                setIsPending(false)
+                setError('failed to upload')
+                console.log(err)
+                setImgSrc(['https://i.ibb.co/g3ffFKB/camera.png'])
+            })
     }
 
     const handleChange = (e) => {
@@ -38,34 +72,123 @@ export default function Post() {
 
     // display image before uploading
     const imagePreview = (event) => {
+        setImgSrc(['https://i.ibb.co/g3ffFKB/camera.png'])
         const allowedExt = /(\.jpg|\.jpeg|\.png)$/i
         if (!allowedExt.exec(event.target.value)) {
             setError(true)
             event.target.value = ''
         } else {
             setError(false)
+            // multiple images
             if (event.target.files && event.target.files[0]) {
-                const reader = new FileReader()
-                reader.readAsDataURL(event.target.files[0])
-                reader.onload = (e) => {
-                    setImgSrc(e.target.result)
+                const files = [...event.target.files]
+                files.map((file) => {
+                    const reader = new FileReader()
+                    reader.readAsDataURL(file)
+                    reader.onload = (e) => {
+                        setImgSrc((prevState) => [
+                            ...prevState,
+                            e.target.result,
+                        ])
 
-                    setPost((prevState) => ({
-                        ...prevState,
-                        image: e.target.result,
-                    }))
-                }
+                        setPost((prevState) => ({
+                            ...prevState,
+                            image: [...prevState.image, e.target.result],
+                        }))
+                    }
+                })
             }
         }
     }
 
+    useEffect(() => {
+        if (error) console.log(error)
+    }, [error])
+
     return (
         <form className='container my-lg-5' onSubmit={handleSubmit}>
             <div className='row text-center'>
-                <div className='col-12 col-md-6  d-flex flex-column justify-content-center align-items-center'>
-                    <div className='m-2 m-md-3' style={imageStyle(width)}>
-                        <img className='img-thumbnail' src={imgSrc} alt='...' />
-                    </div>
+                <div className='col-12 col-md-6 d-flex flex-column justify-content-center align-items-center'>
+                    {error && (
+                        <div
+                            className='alert alert-danger d-flex align-items-center alert-dismissible fade show'
+                            role='alert'>
+                            <i className='bi bi-radioactive me-2'></i>
+                            <div>{error}</div>
+                            <button
+                                type='button'
+                                className='btn-close'
+                                data-bs-dismiss='alert'
+                                aria-label='Close'
+                                onClick={() => setError(false)}></button>
+                        </div>
+                    )}
+
+                    {imgSrc.length > 2 ? (
+                        <div
+                            id='postImageCarousel'
+                            className='carousel slide m-2 m-md-3 border'
+                            data-bs-ride='carousel'
+                            style={carouselStyle(width)}>
+                            <div className='carousel-inner'>
+                                <div className='carousel-item active'>
+                                    <img
+                                        src={
+                                            imgSrc.length > 1
+                                                ? imgSrc[1]
+                                                : imgSrc[0]
+                                        }
+                                        className='img-thumbnail'
+                                        alt='...'
+                                        style={imageStyle(width)}
+                                    />
+                                </div>
+                                {imgSrc.slice(2).map((img, i) => (
+                                    <div key={i} className='carousel-item'>
+                                        <img
+                                            src={img}
+                                            className='img-thumbnail'
+                                            alt='...'
+                                            style={imageStyle(width)}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                            <button
+                                className='carousel-control-prev'
+                                type='button'
+                                data-bs-target='#postImageCarousel'
+                                data-bs-slide='prev'>
+                                <span
+                                    className='carousel-control-prev-icon'
+                                    aria-hidden='true'
+                                />
+                                <span className='visually-hidden'>
+                                    Previous
+                                </span>
+                            </button>
+                            <button
+                                className='carousel-control-next'
+                                type='button'
+                                data-bs-target='#postImageCarousel'
+                                data-bs-slide='next'>
+                                <span
+                                    className='carousel-control-next-icon'
+                                    aria-hidden='true'
+                                />
+                                <span className='visually-hidden'>Next</span>
+                            </button>
+                        </div>
+                    ) : (
+                        <div className='m-2 m-md-3'>
+                            <img
+                                className='img-thumbnail'
+                                src={imgSrc.length > 1 ? imgSrc[1] : imgSrc[0]}
+                                alt='...'
+                                style={imageStyle(width)}
+                            />
+                        </div>
+                    )}
                     <div className='m-3'>
                         <label
                             htmlFor='image'
@@ -74,6 +197,7 @@ export default function Post() {
                         </label>
                         <input
                             type='file'
+                            multiple
                             name='image'
                             id='image'
                             accept='image/*'
@@ -125,7 +249,16 @@ export default function Post() {
                         </div>
                     </div>
                     <div className='mb-3 mt-auto align-self-center align-self-md-end me-3 '>
-                        <button className='btn btn-primary px-5'>Post</button>
+                        {isPending && (
+                            <button className='btn btn-primary px-5'>
+                                Posting...
+                            </button>
+                        )}
+                        {!isPending && (
+                            <button className='btn btn-primary px-5'>
+                                Post
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
